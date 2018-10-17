4a02a50929d8986b97e165f8d6fbc2134db6342b
Jinzi Huang <jinzi.huang@gmail.com>
2018-10-17 18:08:20 +0100

    add bootstrap and react-vis

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
+import LineChart from './components/lineChart.js';
+import BarChart from './components/barChart.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Button bsStyle="primary"> Bootstrap Button</Button>
+          <LineChart/>
+          <BarChart/>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
