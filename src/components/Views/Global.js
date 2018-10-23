8c3c837f1e0335203ef523ebadeb092df932b7b6
Jinzi Huang <jinzi.huang@gmail.com>
2018-10-23 11:58:02 +0100

    Add Tree components

--- src/components/Views/Global.js
+++ src/components/Views/Global.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Pie from '../Charts/Pie';
+import TreeChart from '../Charts/Tree';
+// import '../Charts/Tree.css';
 
 class Global extends Component {
     constructor () {
         super();
         this.state = {
-            global: []
+            // global: [],
+            global: {}
         }
         this.getPatientsGlobal();
       }
@@ -15,12 +18,42 @@ class Global extends Component {
         axios.get('http://localhost:8080/patients/global')
         .then(response => {
             this.setState((state, props) => ({
-                global: [
-                    {angle: response.data.curativeCount, patientType: 'Curative'}, 
-                    {angle: response.data.palliCount, patientType: 'Palli'}, 
-                    {angle: response.data.missCount, patientType: 'Missing'},
-                    {angle: response.data.nACount, patientType: 'N/A'},
-                    ]
+                // global: [
+                //     {angle: response.data.curativeCount, patientType: 'Curative'}, 
+                //     {angle: response.data.palliCount, patientType: 'Palli'}, 
+                //     {angle: response.data.missCount, patientType: 'Missing'},
+                //     {angle: response.data.nACount, patientType: 'N/A'},
+                //     ],
+                global: {
+                    name: 'Patients',
+                    children: [
+                    {
+                        name: 'Curative: ' + response.data.curativeCount,
+                        gProps: {
+                            className: 'red-node',
+                            onClick: node => {
+                                // alert(`Clicked ${node}!`);
+                            }
+                               
+                        }
+                    },
+                    {
+                        name: 'Other',
+                        children: [ 
+                            {
+                                name: 'Palli: ' + response.data.palliCount
+                            },
+                            {
+                                name: 'Missing Data: ' + response.data.missCount
+                            },
+                            {
+                                name: 'N/A: ' + response.data.nACount
+                            }
+                            
+                        ]
+                    }
+                ]
+                }
               }));   
         });
       }
@@ -28,7 +61,8 @@ class Global extends Component {
 
         return (
             <div>
-                {!this.state.isHidden && <Pie data={this.state.global} name={'Global'}></Pie>}
+                {/* <Pie data={this.state.global} name={'Global'}></Pie> */}
+                <TreeChart data={this.state.global}/>
             </div>
         );
       }
