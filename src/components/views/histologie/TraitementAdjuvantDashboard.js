00c37136380d94c721a6140f42aff2e0155e7d30
Jinzi Huang <jinzi.huang@gmail.com>
2018-11-06 12:39:43 +0000

    Add loading for after surgery

--- src/components/views/histologie/TraitementAdjuvantDashboard.js
+++ src/components/views/histologie/TraitementAdjuvantDashboard.js
@@ -6,11 +6,14 @@ import ChemoRCT from './ChemoRCT';
 import TimeBTSurgeryAndAdjuv from './TimeBTSurgeryAndAdjuv';
 import ChemoRCTWithBlood from './ChemoRCTWithBlood';
 import Config from '../../../config/config'
+import loading from '../../imgs/loading.gif';
 
 class TraitementAdjuvant extends Component {
     constructor (props) {
         super(props);
-        
+        this.state = {
+            showLoading: true
+        }
         // API instances 
         this.traitementAdjuvantApi = new TraitementAdjuvantApi();
 
@@ -37,16 +40,23 @@ class TraitementAdjuvant extends Component {
         this.updateFilter();
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         const self = this;
+        const chemoRCT = await self.traitementAdjuvantApi.getChemoRCT(self.params);
+        const chemoRCTDr = await self.traitementAdjuvantApi.getChemoRCT(self.params_dr);
+        const timebwsurgeryAndAdjuv = await self.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(self.params);
+        const timebwsurgeryAndAdjuvDr = await self.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(self.params_dr);
+        const chemoRCTBlood = await self.traitementAdjuvantApi.getChemoRCTBlood(self.params);
+        const chemoRCTBloodDr = await self.traitementAdjuvantApi.getChemoRCTBlood(self.params_dr);
         self.setState({
             drIsHidden: true,
-            chemoRCT: self.traitementAdjuvantApi.getChemoRCT(self.params),
-            chemoRCTDr: self.traitementAdjuvantApi.getChemoRCT(self.params_dr),
-            timebwsurgeryAndAdjuv: self.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(self.params),
-            timebwsurgeryAndAdjuvDr: self.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(self.params_dr),
-            chemoRCTBlood: self.traitementAdjuvantApi.getChemoRCTBlood(self.params),
-            chemoRCTBloodDr: self.traitementAdjuvantApi.getChemoRCTBlood(self.params_dr),
+            chemoRCT,
+            chemoRCTDr,
+            timebwsurgeryAndAdjuv,
+            timebwsurgeryAndAdjuvDr,
+            chemoRCTBlood,
+            chemoRCTBloodDr,
+            showLoading: false
         })
     }
 
@@ -78,63 +88,80 @@ class TraitementAdjuvant extends Component {
                 default:        
             }
         }
+
+        const chemoRCT = await this.traitementAdjuvantApi.getChemoRCT(this.params);
+        const chemoRCTDr = await this.traitementAdjuvantApi.getChemoRCT(this.params_dr);
+        const timebwsurgeryAndAdjuv = await this.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(this.params);
+        const timebwsurgeryAndAdjuvDr = await this.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(this.params_dr);
+        const chemoRCTBlood = await this.traitementAdjuvantApi.getChemoRCTBlood(this.params);
+        const chemoRCTBloodDr = await this.traitementAdjuvantApi.getChemoRCTBlood(this.params_dr);
+
         this.setState({
-            chemoRCT: await this.traitementAdjuvantApi.getChemoRCT(this.params),
-            chemoRCTDr: await this.traitementAdjuvantApi.getChemoRCT(this.params_dr),
-            timebwsurgeryAndAdjuv: await this.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(this.params),
-            timebwsurgeryAndAdjuvDr:  await this.traitementAdjuvantApi.getTimebwsurgeryAndAdjuv(this.params_dr),
-            chemoRCTBlood: await this.traitementAdjuvantApi.getChemoRCTBlood(this.params),
-            chemoRCTBloodDr: await this.traitementAdjuvantApi.getChemoRCTBlood(this.params_dr),
+            chemoRCT,
+            chemoRCTDr,
+            timebwsurgeryAndAdjuv,
+            timebwsurgeryAndAdjuvDr,
+            chemoRCTBlood,
+            chemoRCTBloodDr,
+            showLoading: false
         });
     }
 
     render() {
+        let Content;
+        if (this.state.showLoading) {
+            Content = 
+            <Row><Col xs={10} md={10}><div className="loading"><img src={loading} alt="loading..."/></div></Col></Row>
+        } else {
+            Content = 
+            <Row>
+                <Col xs={4} md={4}>
+                { this.state && this.state.chemoRCT && 
+                    this.state.chemoRCT.total &&
+                    this.state.chemoRCTDr &&
+                    <ChemoRCT 
+                        data={this.state.chemoRCT}
+                        hide_dr={this.state.drIsHidden}
+                        dr={this.state.chemoRCTDr}
+                    />
+                }
+                </Col>
+                <Col xs={4} md={4}>
+                { this.state && this.state.timebwsurgeryAndAdjuv && 
+                    this.state.timebwsurgeryAndAdjuv.data &&
+                    this.state.timebwsurgeryAndAdjuvDr &&
+                    <TimeBTSurgeryAndAdjuv 
+                    data={this.state.timebwsurgeryAndAdjuv.data}
+                    total={this.state.timebwsurgeryAndAdjuv.total}
+                    lessth6weeks={this.state.timebwsurgeryAndAdjuv.lessth6weeks}
+                    morethan6weeks={this.state.timebwsurgeryAndAdjuv.morethan6weeks}
+                    missing={this.state.timebwsurgeryAndAdjuv.missing}
+                    dr_lessth6weeks= {this.state.timebwsurgeryAndAdjuvDr.lessth6weeks}
+                    dr_morethan6weeks= {this.state.timebwsurgeryAndAdjuvDr.morethan6weeks}
+                    dr_missing= {this.state.timebwsurgeryAndAdjuvDr.missing}
+                    dr_total= {this.state.timebwsurgeryAndAdjuvDr.total}
+                    hide_dr={this.state.drIsHidden}
+                    />
+                }
+                </Col>
+                <Col xs={4} md={4}>
+                { this.state && this.state.chemoRCTBlood && 
+                    this.state.chemoRCTBlood.total &&
+                    this.state.chemoRCTBloodDr &&
+                    <ChemoRCTWithBlood 
+                        data={this.state.chemoRCTBlood}
+                        hide_dr={this.state.drIsHidden}
+                        dr={this.state.chemoRCTBloodDr}
+                    />
+                }
+                </Col>
+            </Row>
+        }
         return (
             <div>
                 <Filter updateFilter = {this.updateFilter}/>
                 <Grid className ='patient_grid'>
-                    <Row>
-                        <Col xs={4} md={4}>
-                        { this.state && this.state.chemoRCT && 
-                            this.state.chemoRCT.total &&
-                            this.state.chemoRCTDr &&
-                            <ChemoRCT 
-                                data={this.state.chemoRCT}
-                                hide_dr={this.state.drIsHidden}
-                                dr={this.state.chemoRCTDr}
-                            />
-                        }
-                        </Col>
-                        <Col xs={4} md={4}>
-                        { this.state && this.state.timebwsurgeryAndAdjuv && 
-                            this.state.timebwsurgeryAndAdjuv.data &&
-                            this.state.timebwsurgeryAndAdjuvDr &&
-                            <TimeBTSurgeryAndAdjuv 
-                            data={this.state.timebwsurgeryAndAdjuv.data}
-                            total={this.state.timebwsurgeryAndAdjuv.total}
-                            lessth6weeks={this.state.timebwsurgeryAndAdjuv.lessth6weeks}
-                            morethan6weeks={this.state.timebwsurgeryAndAdjuv.morethan6weeks}
-                            missing={this.state.timebwsurgeryAndAdjuv.missing}
-                            dr_lessth6weeks= {this.state.timebwsurgeryAndAdjuvDr.lessth6weeks}
-                            dr_morethan6weeks= {this.state.timebwsurgeryAndAdjuvDr.morethan6weeks}
-                            dr_missing= {this.state.timebwsurgeryAndAdjuvDr.missing}
-                            dr_total= {this.state.timebwsurgeryAndAdjuvDr.total}
-                            hide_dr={this.state.drIsHidden}
-                            />
-                        }
-                        </Col>
-                        <Col xs={4} md={4}>
-                        { this.state && this.state.chemoRCTBlood && 
-                            this.state.chemoRCTBlood.total &&
-                            this.state.chemoRCTBloodDr &&
-                            <ChemoRCTWithBlood 
-                                data={this.state.chemoRCTBlood}
-                                hide_dr={this.state.drIsHidden}
-                                dr={this.state.chemoRCTBloodDr}
-                            />
-                        }
-                        </Col>
-                    </Row>
+                {Content}
                 </Grid>
             </div>
         )
