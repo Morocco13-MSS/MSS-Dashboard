fde467711b0595d29769be75bd33e1a960708b22
Jinzi Huang <jinzi.huang@gmail.com>
2018-11-06 16:10:02 +0000

    Code Clean up

--- src/components/views/welcome/Login.js
+++ src/components/views/welcome/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {ControlLabel, FormGroup, FormControl, Button, Col, Form} from 'react-bootstrap';
 import './Welcome.css';
-import Config from '../../../config/config';
 import axios from 'axios';
 
 class Login extends Component {
--- src/components/views/welcome/Welcome.js
+++ src/components/views/welcome/Welcome.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, Grid, Row, Col, Glyphicon, NavDropdown, MenuItem, Media, Form, FormGroup, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Grid, Row, Col, Glyphicon, NavDropdown, MenuItem, Media} from 'react-bootstrap';
 import Login from './Login'
 
 import './Welcome.css'
@@ -60,7 +60,7 @@ class Welcome extends Component {
 
 
                             <div>
-                                <div class="firstTitle">
+                                <div className="firstTitle">
                                     <h3>Résumé du protocole</h3>
                                 </div>
                                 <Media>
@@ -83,7 +83,7 @@ class Welcome extends Component {
                                     <Media.Body>
                                         <Media.Heading>Promoteur</Media.Heading>
                                         <p>
-                                            <a class="cBlack" href="http://www.somachir.com">Société Marocaine de Chirurgie (www.somachir.com)</a>
+                                            <a className="cBlack" href="http://www.somachir.com">Société Marocaine de Chirurgie (www.somachir.com)</a>
                                         </p>
                                     </Media.Body>
                                 </Media>
@@ -116,7 +116,7 @@ class Welcome extends Component {
                                 <Media>
                                     <Media.Body>
                                         <Media.Heading>Rationnel et justification</Media.Heading>
-                                        <p class="justify">
+                                        <p className="justify">
                                         La chirurgie occupe une place centrale dans les stratégies thérapeutiques curatives des cancers digestifs. Elle se caractérise par une complexité́, une morbidité́ et des coûts importants. Il existe un contraste notable entre l'existence de normes acceptées de qualité́ chirurgicale (référentiels nationaux ou régionaux de bonne pratique en chirurgie cancérologique) et la disparité́ des résultats enregistrés. En complément à une stratégie de concentration des cas complexes dans des centres à haut volume d’activité, la disparité des résultats de la chirurgie a justifié la mise en place par des sociétés savantes européennes de systèmes d’assurance qualité́ dédiés à une ou plusieurs localisations de cancer digestif. L’analyse de ces expériences a montré que l’implémentation de registres d’audit chirurgical sans mesure interventionnelle spécifique initiale avait un impact positif, rapide et coût-efficace sur les taux de complications, les taux de récidive ainsi que sur la survie globale. L’analyse des données recueillies a par ailleurs permis d’améliorer les connaissances et donc la prise en charge de sous-groupes de patients habituellement exclus des essais cliniques.
                                             Au Maroc, l’équité et la qualité sont deux des valeurs retenues par Plan National de Prévention et de Contrôle du Cancer (PNPCC, 2010-2019). Ces valeurs impliquent respectivement le développement «d’une offre de soins identiques pour tous et accessibles» et «d’une prise en charge complète répondant au normes». D’un point de vue opérationnel, les mesures 53 et 54 du PNPCC prévoient la mise en place d’un système d’assurance qualité́ avec l’instauration d’un système de suivi et d’évaluation 
                                             de la prise en charge des patients. 
@@ -129,7 +129,7 @@ class Welcome extends Component {
                                         <Media.Heading>
                                             Schéma de l'étude
                                         </Media.Heading>
-                                        <p class="justify">
+                                        <p className="justify">
                                         Etude clinique prospective multicentrique observationnelle, évaluant la qualité des actes de chirurgie pour cancers digestifs prouvés ou suspectés (intention de traiter).
                                         </p>
                                     </Media.Body>
@@ -140,7 +140,7 @@ class Welcome extends Component {
                                         <Media.Heading>
                                         Nombre de patients 
                                         </Media.Heading>
-                                        <p class="justify">
+                                        <p className="justify">
                                         Nombre total de patients attendus: 1500 (500 patients/an sur 3 ans d’étude) 
                                         
                                         </p>
@@ -152,7 +152,7 @@ class Welcome extends Component {
                                         <Media.Heading>
                                         Objectifs de l’étude
                                         </Media.Heading>
-                                        <p class="justify">
+                                        <p className="justify">
                                         Evaluer la qualité des résultats de la chirurgie pour cancers digestifs dans les centres investigateurs.
 
                                         </p>
@@ -163,7 +163,7 @@ class Welcome extends Component {
                                     <Media.Body>
                                         <Media.Heading>
                                         Population de patients de l’étude                                        </Media.Heading>
-                                        <p class="justify">
+                                        <p className="justify">
                                         Patients de sexes masculin et féminin âgés de 16 ans et plus opérés dans un des centres investigateurs pour un cancer de localisation digestive prouvé ou suspecté, à visée curative ou palliative, en situation élective ou urgente.
                                         </p>
                                     </Media.Body>
@@ -176,12 +176,12 @@ class Welcome extends Component {
                                         <Media.Heading>
                                         Principaux critères d’inclusion 
                                         </Media.Heading>
-                                        <ul class="noStyleList">
-                                            <li class="justify"><span class="margin5R">1.</span>Patient de sexes masculin et féminin âgés de 16 ans et plus opéré pour  un cancer de localisation digestive, prouvé ou suspecté, à visée curative ou palliative</li>
-                                            <ol class="justify"><span class="margin5R">1.</span>En situation élective: inclusion à partir de la programmation pour chirurgie</ol>
-                                            <ol class="justify"><span class="margin5R">2.</span>En situation urgente: inclusion au plus tard 72 heures après la  chirurgie</ol>
-                                            <li class="justify"><span class="margin5R">2.</span>Cancer prouvé ou suspecté dans les localisations digestives suivantes: colon, appendice, anus, rectum, oesophage, estomac, jonction oeso-gastrique, voies biliaires, ampoule de Vater, pancréas, duodénum,  intestin grêle et foie.</li>
-                                            <li class="justify"><span class="margin5R">3.</span>Patient disposé et apte à donner son accord pour participer à l’étude.</li>
+                                        <ul className="noStyleList">
+                                            <li className="justify"><span className="margin5R">1.</span>Patient de sexes masculin et féminin âgés de 16 ans et plus opéré pour  un cancer de localisation digestive, prouvé ou suspecté, à visée curative ou palliative</li>
+                                            <ol className="justify"><span className="margin5R">1.</span>En situation élective: inclusion à partir de la programmation pour chirurgie</ol>
+                                            <ol className="justify"><span className="margin5R">2.</span>En situation urgente: inclusion au plus tard 72 heures après la  chirurgie</ol>
+                                            <li className="justify"><span className="margin5R">2.</span>Cancer prouvé ou suspecté dans les localisations digestives suivantes: colon, appendice, anus, rectum, oesophage, estomac, jonction oeso-gastrique, voies biliaires, ampoule de Vater, pancréas, duodénum,  intestin grêle et foie.</li>
+                                            <li className="justify"><span className="margin5R">3.</span>Patient disposé et apte à donner son accord pour participer à l’étude.</li>
                                         </ul>
 
 
@@ -194,20 +194,20 @@ class Welcome extends Component {
                                         <Media.Heading>
                                         Principaux critères d’exclusion 
                                         </Media.Heading>
-                                        <ul class="noStyleList">
-                                            <li class="justify "><span class="margin5R">1.</span>Patient dont l’intervention chirurgicale est indiquée pour:</li>
-                                            <ol class="justify deco1List">
+                                        <ul className="noStyleList">
+                                            <li className="justify "><span className="margin5R">1.</span>Patient dont l’intervention chirurgicale est indiquée pour:</li>
+                                            <ol className="justify deco1List">
                                             <li>Une affection qui n’est pas un cancer de localisation digestive, 
                                                 y compris si rétrospectivement l’exploration chirurgicale et/ou l’examen histologique révèle un cancer de localisation 
                                                 digestive</li>
                                             <li>Un cancer prouvé ou suspecté de localisation non digestive</li>
                                             <li>Un cancer prouvé ou suspecté de localisation péritonéale</li>
                                             </ol>
-                                            <li class="justify"><span class="margin5R">2.</span>Patient dont l’intervention chirurgicale est indiquée pour une poursuite évolutive ou une récidive locale prouvée ou suspectée d’un cancer de localisation digestive ayant déjà fait l’objet d’une intervention chirurgicale (à l’exception des situations d’hépatectomie itérative pour métastase hépatique et de reprise du lit tumoral après découverte d’un cancer vésiculaire sur pièce de cholécystectomie).
+                                            <li className="justify"><span className="margin5R">2.</span>Patient dont l’intervention chirurgicale est indiquée pour une poursuite évolutive ou une récidive locale prouvée ou suspectée d’un cancer de localisation digestive ayant déjà fait l’objet d’une intervention chirurgicale (à l’exception des situations d’hépatectomie itérative pour métastase hépatique et de reprise du lit tumoral après découverte d’un cancer vésiculaire sur pièce de cholécystectomie).
                                             </li>
-                                            <li class="justify"><span class="margin5R">3.</span>Patients dont l’intervention est à visée diagnostique sans aucune intention curative ou palliative</li>
-                                            <li class="justify"><span class="margin5R">4.</span>Patients dont l’intervention chirurgicale est une transplantation hépatique.</li>
-                                            <li class="justify"><span class="margin5R">5.</span>Patients dont l’intervention chirurgicale est une destruction locale (radiofréquence, micro-ondes) par voie percutanée exclusive (sans laparotomie ou laparoscopie).</li>
+                                            <li className="justify"><span className="margin5R">3.</span>Patients dont l’intervention est à visée diagnostique sans aucune intention curative ou palliative</li>
+                                            <li className="justify"><span className="margin5R">4.</span>Patients dont l’intervention chirurgicale est une transplantation hépatique.</li>
+                                            <li className="justify"><span className="margin5R">5.</span>Patients dont l’intervention chirurgicale est une destruction locale (radiofréquence, micro-ondes) par voie percutanée exclusive (sans laparotomie ou laparoscopie).</li>
 
 
                                         </ul>
@@ -217,7 +217,7 @@ class Welcome extends Component {
                                     </Media.Body>
                                 </Media>
 
-                                <div class="firstTitle">
+                                <div className="firstTitle">
                                     <h3>Critères d’évaluation de l’étude clinique</h3>
                                 </div>
                             
